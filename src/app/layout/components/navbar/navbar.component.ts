0b35d4de8107d4f969f7e466613fff9cc95fbfc6
Bruno Capitelli <bruno.capitelli@salu.com.vc>
2023-09-20 09:43:05 -0300

    -feat: initing navbar; -style: utils.scss;

--- src/app/layout/components/navbar/navbar.component.ts
+++ src/app/layout/components/navbar/navbar.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-
+  navItems = [
+    {
+      title: 'Agents',
+      path: 'agents'
+    },
+    {
+      title: 'Weapons',
+      path: 'weapons'
+    },
+  ]
 }
