1e5218b502602e7ee5829e58341e12b0fc8da896
Bruno Capitelli <bruno.capitelli@salu.com.vc>
2023-09-21 08:18:05 -0300

    -feat: navbar;

--- src/app/layout/components/navbar/navbar.component.ts
+++ src/app/layout/components/navbar/navbar.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements AfterViewInit {
+  @ViewChildren('navItem') navItemsRefs!: QueryList<ElementRef<HTMLDivElement>>
+  @ViewChild('seekingBox') seekingBox!: ElementRef<HTMLDivElement>
   navItems = [
     {
       title: 'Agents',
@@ -15,5 +17,33 @@ export class NavbarComponent {
       title: 'Weapons',
       path: 'weapons'
     },
+    {
+      title: 'Maps',
+      path: 'maps'
+    },
   ]
+
+  ngAfterViewInit(): void {
+    this.navItemsRefs.forEach(({ nativeElement }, i, items) => {
+      nativeElement.addEventListener('mouseover', () => {
+        this.seekingBox.nativeElement.style.left = nativeElement.offsetLeft + 10 + 'px'
+
+        items.forEach((item) => {
+          if(item.nativeElement != nativeElement) {
+            item.nativeElement.style.opacity = '0.5'
+          }
+        })
+      })
+
+      nativeElement.addEventListener('mouseout', () => {
+        this.seekingBox.nativeElement.style.left = '90vw'
+
+        items.forEach((item) => {
+          if(item.nativeElement != nativeElement) {
+            item.nativeElement.style.opacity = '1'
+          }
+        })
+      })
+    })
+  }
 }
