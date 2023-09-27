52785e77112d57c45d884e1369cdcfb3f9146923
Bruno Capitelli <bruno.capitelli@salu.com.vc>
2023-09-27 17:54:44 -0300

    -feat: weapons listing;

--- src/app/pages/weapons/weapons.component.ts
+++ src/app/pages/weapons/weapons.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Weapon } from '@interfaces/Weapon';
 import { WeaponService } from '@services/weapon.service';
-import { tap, switchMap, map } from 'rxjs';
+import { map, Observable, forkJoin, concatMap } from 'rxjs';
 
 @Component({
   selector: 'app-weapons',
@@ -8,12 +9,16 @@ import { tap, switchMap, map } from 'rxjs';
   styleUrls: ['./weapons.component.scss']
 })
 export class WeaponsComponent implements OnInit {
-  weaponImg$ = this.weaponService.list().pipe(
-    tap(weapons => console.log(weapons)),
-    switchMap(([ first ]) => this.weaponService.getMedia(first.displayIcon))
-  )
+  weapons$!: Observable<Weapon[]>
+  weaponsIcons$!: Observable<string[]>
 
-  constructor(private weaponService: WeaponService) {}
+  constructor(private weaponService: WeaponService) {
+    this.weapons$ = this.weaponService.list()
+    this.weaponsIcons$ = this.weapons$.pipe(
+      map(weapons => weapons.map(({ displayIcon }) => this.weaponService.getMedia(displayIcon))),
+      concatMap(getMedias => forkJoin(getMedias)),
+    )
+  }
 
   ngOnInit(): void {
     // this.weapons$.subscribe()
