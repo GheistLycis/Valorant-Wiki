8d01409442436c111b9ba54ef23d84a022d7d3f4
Bruno Capitelli <bruno.capitelli@salu.com.vc>
2023-10-03 12:45:05 -0300

    -chore: chart components typing;

--- src/app/pages/weapons/components/weapons-match/weapons-match.component.ts
+++ src/app/pages/weapons/components/weapons-match/weapons-match.component.ts
@@ -3,7 +3,9 @@ import { palette } from '@enums/palette';
 import { Weapon } from '@interfaces/Weapon';
 import { NgbNavConfig } from '@ng-bootstrap/ng-bootstrap';
 import { WeaponService } from '@services/weapon.service';
-import { ChartConfiguration } from 'chart.js';
+import { ChartData, ChartDataset, ChartOptions } from 'chart.js';
+
+
 
 @Component({
   selector: 'app-weapons-match',
@@ -13,32 +15,8 @@ import { ChartConfiguration } from 'chart.js';
 export class WeaponsMatchComponent {
   $weapons!: WritableSignal<Weapon[]>
   $selectedWeapon!: WritableSignal<Weapon['uuid']>
-  $data!: Signal<any>
-  options: ChartConfiguration<'radar'>['options'] = {
-    color: palette.black,
-    plugins: {
-      legend: {
-        display: false,
-      },
-      tooltip: {
-        callbacks: {
-          title: ([ tooltip ]) => tooltip.dataset.label!.toUpperCase(),
-          label: ({ parsed }) => parsed.r.toString(),
-        },
-        titleFont: {
-          family: 'secondary',
-          size: 20,
-        },
-        bodyFont: {
-          family: 'secondary',
-          size: 40,
-          weight: '700',
-        },
-        boxHeight: 0,
-        boxWidth: 0,
-      },
-    },
-  }
+  $data!: Signal<ChartData<'radar', number[], string>>
+  options!: ChartOptions<'radar'>
 
   constructor(
     public weaponService: WeaponService,
@@ -61,21 +39,52 @@ export class WeaponsMatchComponent {
         'Reload Time (s)',
         'Equip Time (s)',
       ],
-      datasets: this.$weapons().map(({ displayName, weaponStats, uuid }) => ({
-        label: displayName,
-        data: [
-          (weaponStats!.damageRanges.reduce((acc, { headDamage }) => acc += headDamage, 0)) / (weaponStats!.damageRanges.length),
-          (weaponStats!.damageRanges.reduce((acc, { bodyDamage }) => acc += bodyDamage, 0)) / (weaponStats!.damageRanges.length),
-          (weaponStats!.damageRanges.reduce((acc, { legDamage }) => acc += legDamage, 0)) / (weaponStats!.damageRanges.length),
-          weaponStats!.magazineSize,
-          weaponStats!.fireRate,
-          weaponStats!.damageRanges.reduce((acc, { rangeEndMeters }) => acc = Math.max(rangeEndMeters, acc), 0),
-          weaponStats!.reloadTimeSeconds,
-          weaponStats!.equipTimeSeconds,
-        ],
-        fill: true,
-        order: uuid == this.$selectedWeapon() ? 0 : 1,
-      }))
+      datasets: this.$weapons().map(({ displayName, weaponStats, uuid }) => {
+        const { damageRanges, magazineSize, fireRate, reloadTimeSeconds, equipTimeSeconds } = weaponStats!
+        const dataset: ChartDataset<'radar', number[]> = {
+          label: displayName,
+          data: [
+            (damageRanges.reduce((acc, { headDamage }) => acc += headDamage, 0)) / (damageRanges.length),
+            (damageRanges.reduce((acc, { bodyDamage }) => acc += bodyDamage, 0)) / (damageRanges.length),
+            (damageRanges.reduce((acc, { legDamage }) => acc += legDamage, 0)) / (damageRanges.length),
+            magazineSize,
+            fireRate,
+            damageRanges.reduce((acc, { rangeEndMeters }) => acc = Math.max(rangeEndMeters, acc), 0),
+            reloadTimeSeconds,
+            equipTimeSeconds,
+          ],
+          fill: true,
+          order: uuid == this.$selectedWeapon() ? 0 : 1,
+        }
+
+        return dataset
+      })
     }))
+
+    this.options = {
+      color: palette.black,
+      plugins: {
+        legend: {
+          display: false,
+        },
+        tooltip: {
+          callbacks: {
+            title: ([ tooltip ]) => tooltip.dataset.label!.toUpperCase(),
+            label: ({ parsed }) => parsed.r.toString(),
+          },
+          titleFont: {
+            family: 'secondary',
+            size: 20,
+          },
+          bodyFont: {
+            family: 'secondary',
+            size: 40,
+            weight: '700',
+          },
+          boxHeight: 0,
+          boxWidth: 0,
+        },
+      },
+    }
   }
 }
