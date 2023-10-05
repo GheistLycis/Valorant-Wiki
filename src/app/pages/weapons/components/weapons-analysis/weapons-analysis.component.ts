c969fd9f0f1e76cba77dafdb399482cfae5b7115
Bruno Capitelli <bruno.capitelli@salu.com.vc>
2023-10-05 12:28:03 -0300

    -style: chart; -style: scrollbar;

--- src/app/pages/weapons/components/weapons-analysis/weapons-analysis.component.ts
+++ src/app/pages/weapons/components/weapons-analysis/weapons-analysis.component.ts
@@ -6,7 +6,6 @@ import { WeaponService } from '@services/weapon.service';
 import { ChartData, ChartDataset, ChartOptions } from 'chart.js';
 
 
-
 @Component({
   selector: 'app-weapons-analysis',
   templateUrl: './weapons-analysis.component.html',
@@ -14,20 +13,19 @@ import { ChartData, ChartDataset, ChartOptions } from 'chart.js';
 })
 export class WeaponsAnalysisComponent {
   $weapons!: WritableSignal<Weapon[]>
-  $selectedWeapon!: WritableSignal<Weapon['uuid']>
+  $selectedWeapon!: WritableSignal<Weapon>
   $data!: Signal<ChartData<'radar', number[], string>>
   options!: ChartOptions<'radar'>
+  $advantages!: Signal<string[]>
 
   constructor(
     public weaponService: WeaponService,
-    private navConfig: NgbNavConfig
+    private navConfig: NgbNavConfig,
   ) {
     navConfig.animation = true
 
     this.$weapons = weaponService.$selectedWeapons
-
-    this.$selectedWeapon = signal(this.$weapons()[0].uuid)
-
+    this.$selectedWeapon = signal(this.$weapons()[0])
     this.$data = computed(() => ({
       labels: [
         'AVG DMG Head',
@@ -53,24 +51,55 @@ export class WeaponsAnalysisComponent {
             reloadTimeSeconds,
             equipTimeSeconds,
           ],
-          fill: true,
-          order: uuid == this.$selectedWeapon() ? 0 : 1,
+          order: uuid == this.$selectedWeapon().uuid ? 0 : 1,
         }
 
         return dataset
       })
     }))
-
     this.options = {
-      color: palette.black,
+      elements: {
+        line: {
+          borderWidth: 3,
+          tension: 0.5,
+        },
+        point: {
+          borderWidth: 0,
+        }
+      },
+      scales: {
+        r: {
+          grid: {
+            circular: true,
+            lineWidth: 2,
+          },
+          ticks: {
+            backdropColor: 'transparent',
+            color: 'rgba(0, 0, 0, 0.5)',
+          },
+          pointLabels: {
+            color: palette['dark-gray'],
+            font: {
+              family: 'secondary',
+              size: 15,
+            }
+          },
+        },
+      },
       plugins: {
         legend: {
-          display: false,
+          position: 'bottom',
+          labels: {
+            color: palette['dark-gray'],
+            padding: 20,
+            usePointStyle: true,
+            pointStyle: 'circle',
+          },
         },
         tooltip: {
           callbacks: {
             title: ([ tooltip ]) => tooltip.dataset.label!.toUpperCase(),
-            label: ({ parsed }) => parsed.r.toString(),
+            label: ({ parsed, label }) => `${parsed.r.toFixed(2)} ${label.match(/(?<=\()(.*?)(?=\))/)?.[0] || ''}`, // GET MEASURE UNITY IF ANY
           },
           titleFont: {
             family: 'secondary',
@@ -78,13 +107,16 @@ export class WeaponsAnalysisComponent {
           },
           bodyFont: {
             family: 'secondary',
-            size: 40,
+            size: 35,
             weight: '700',
           },
           boxHeight: 0,
           boxWidth: 0,
         },
-      },
+      }
     }
+    this.$advantages = computed(() => {
+      return Array(200).fill('test')
+    })
   }
 }
