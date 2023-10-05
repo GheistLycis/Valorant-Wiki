82f933b19e0d8423424acc12620512964828c429
Bruno Capitelli <bruno.capitelli@salu.com.vc>
2023-10-05 17:19:24 -0300

    -feat: initing weapons comparison listing;

--- src/app/pages/weapons/components/weapons-analysis/weapons-analysis.component.ts
+++ src/app/pages/weapons/components/weapons-analysis/weapons-analysis.component.ts
@@ -1,8 +1,9 @@
 import { Component, Signal, WritableSignal, computed, signal } from '@angular/core';
 import { Weapon } from '@interfaces/Weapon';
-import { NgbNavConfig } from '@ng-bootstrap/ng-bootstrap';
 import { WeaponService } from '@services/weapon.service';
 
+
+type advantage = { title: string, detail: string }
 
 @Component({
   selector: 'app-weapons-analysis',
@@ -12,18 +13,37 @@ import { WeaponService } from '@services/weapon.service';
 export class WeaponsAnalysisComponent {
   $weapons!: WritableSignal<Weapon[]>
   $selectedWeapon!: WritableSignal<Weapon>
-  $advantages!: Signal<string[]>
+  $advantages!: Signal<advantage[]>
 
-  constructor(
-    public weaponService: WeaponService,
-    private navConfig: NgbNavConfig,
-  ) {
-    navConfig.animation = true
-
+  constructor(public weaponService: WeaponService) {
     this.$weapons = weaponService.$selectedWeapons
     this.$selectedWeapon = signal(this.$weapons()[0])
-    this.$advantages = computed(() => {
-      return Array(5).fill('test')
+    this.$advantages = computed(() => this.getAdvantages())
+  }
+
+  getAdvantages(): advantage[] {
+    const advantages: advantage[] = []
+    const selected = this.$selectedWeapon()
+    const others = this.$weapons().filter(({ uuid }) => uuid != selected.uuid)
+    
+    this.checkSkinsNumber(selected, others, advantages)
+
+    return advantages
+  }
+
+  checkSkinsNumber(selected: Weapon, others: Weapon[], list: advantage[]): void {
+    const advantage = { title: 'More skins available', detail: '' }
+    const selectedSkins = selected.skins.length
+    const othersSkins = others.map(({ displayName, skins }) => ({ weapon: displayName, number: skins.length }))
+
+    othersSkins.forEach(({ weapon, number }) => {
+      if(selectedSkins > number) {
+        if(!advantage.detail) advantage.detail = `<b>${selectedSkins}</b>`
+        
+        advantage.detail += ` vs <b>${number} (${weapon})</b>`
+      }
     })
+
+    if(advantage.detail) list.push(advantage)
   }
 }
